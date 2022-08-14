4b082376c1ffd6e39a1eae8cd8c616f384eca4b8
Franklin Méndez <franklinjosue.net@gmail.com>
2022-08-14 11:22:12 -0600

    script updated

--- project8-number-guesser/script.js
+++ project8-number-guesser/script.js
@@ -7,9 +7,32 @@ const generateTarget = () => {
     return Math.floor(Math.random()* 10);
 }
 
-console.log(generateTarget());
+// console.log(generateTarget());
 
 
-const compareGuesses = (human, computer, secret) => {
-
+const compareGuesses = (human, computer, target) => {
+    const humanDiff = Math.abs(target - human);
+    const computerDiff = Math.abs(target - computer);
+    if (human > 10) {
+        alert('Max number allowed is 10');
+    } else {
+    if (humanDiff < computerDiff) {
+        return true;
+    } else {
+        return false;
+    }
+} 
 }
+
+
+//console.log(compareGuesses()); 
+
+const updateScore = winner => {
+    if (winner === 'human') {
+        humanScore +=1;
+    } else if (winner === 'computer') {
+        computerScore =+1;
+    }
+}
+
+const advanceRound = () => currentRoundNumber +=1;
