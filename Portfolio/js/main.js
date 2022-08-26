d10bfe63ccf9d36a44e0e244eef62d9cc4e0ba33
Franklin Méndez <franklinjosue.net@gmail.com>
2022-08-25 20:59:21 -0600

    Web sections finished

--- Portfolio/js/main.js
+++ Portfolio/js/main.js
@@ -10,3 +10,27 @@ sematicDiv.addEventListener('mouseout', () => {
     document.getElementById('sematic-title').style.visibility = 'hidden';
     document.getElementById('sematic-subtitle').style.visibility = 'hidden';
 })
+
+const atextoLandingDiv = document.getElementById('atextolanding');
+
+atextoLandingDiv.addEventListener('mouseover', () => {
+    document.getElementById('atextolanding-title').style.visibility = 'visible';
+    document.getElementById('atextolanding-subtitle').style.visibility = 'visible';
+})
+
+atextoLandingDiv.addEventListener('mouseout', () => {
+    document.getElementById('atextolanding-title').style.visibility = 'hidden';
+    document.getElementById('atextolanding-subtitle').style.visibility = 'hidden';
+})
+
+const atextoWebDiv = document.getElementById('atexto-web');
+
+atextoWebDiv.addEventListener('mouseover', () => {
+    document.getElementById('atextoweb-title').style.visibility = 'visible';
+    document.getElementById('atextoweb-subtitle').style.visibility = 'visible';
+})
+
+atextoWebDiv.addEventListener('mouseout', () => {
+    document.getElementById('atextoweb-title').style.visibility = 'hidden';
+    document.getElementById('atextoweb-subtitle').style.visibility = 'hidden';
+})
