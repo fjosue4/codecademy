1cd0b130c1c9ac5527e9c7acb7b4d27c0ee72fec
Franklin Méndez <franklinjosue.net@gmail.com>
2022-08-30 13:04:20 -0600

    Responsive v1

--- Portfolio/js/main.js
+++ Portfolio/js/main.js
@@ -11,18 +11,25 @@ let closeModal = document.getElementsByClassName('close');
 for (let i = 0; i < projectDiv.length; i++) {
     projectDiv[i].addEventListener("mouseover", () => {
         divTextAndButton[i].style.visibility = 'visible';
-        })
+        });
     projectDiv[i].addEventListener("mouseout", () => {
         divTextAndButton[i].style.visibility = 'hidden';
-        })
+        });
 
     projectDiv[i].addEventListener("click", () => {
         projectModal[i].style.display = "block";
         divTextAndButton[i].style.visibility = 'hidden';
-            })
+            });
     }
 
 for (let j = 0; j < closeModal.length; j++) {
 closeModal[j].addEventListener('click', () => {
     projectModal[j].style.display = 'none';
-} )}
+} );
+document.addEventListener('keydown', function(event){
+	if(event.key === "Escape"){
+		projectModal[j].style.display = 'none';
+	}
+});
+}
+
